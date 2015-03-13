9100e01a7f8c50c3d7fef5755263d546dd26f5d4
kamikazekip <ebrandsm@pc131135.local>
2015-03-13 16:24:26 +0100

    Safe commit voordat Joost alles verneukt
    en dat ik het dan weer ontneuk

--- snschat/snschat/ChatsController.swift
+++ snschat/snschat/ChatsController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ChatsController: UIViewController {
+class ChatsController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var chat: NSMutableArray! = NSMutableArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.chat.addObject("This is some text")
+        self.chat.addObject("This is also some text, but then some larger so whe can test this application :D some text also in here to make this line a lot lot longer :D")
         
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 44.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,22 +28,20 @@ class ChatsController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.chat.count
+    }
     
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = chat.objectAtIndex(indexPath.row) as? String
+        //cell.textLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        //cell.detailTextLabel?.text = "Hallo :)"
+        return cell
+    }
     
-
     @IBAction func onTapMainView(sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
